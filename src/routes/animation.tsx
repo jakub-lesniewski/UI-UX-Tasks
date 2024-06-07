be57acb083c5b87d3527c9a9f884380eccd98989
Jakub Leśniewski <jakubjanlesniewski@gmail.com>
2024-06-07 10:30:14 +0200

    added carousel and progress bar

--- src/routes/animation.tsx
+++ src/routes/animation.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
 import {
   Dialog,
   DialogContent,
@@ -15,8 +16,23 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { useEffect, useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Animation() {
+  const [progress, setProgress] = useState(13);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="mt-20 flex w-[350px] flex-col items-start rounded-md border-2 p-6">
       <p className="leading-7 [&:not(:first-child)]:mt-4">
@@ -28,7 +44,7 @@ export default function Animation() {
         się w prawym górnym rogu.
       </p>
 
-      <div className="flex flex-col items-start gap-4">
+      <div className="flex flex-col items-start gap-6">
         <div className="mt-6 flex flex-col gap-2">
           <Dialog>
             <DialogTrigger>
@@ -64,7 +80,18 @@ export default function Animation() {
             </SheetContent>
           </Sheet>
         </div>
+
+        <Progress value={progress} className="w-full" />
       </div>
+      <Carousel className="mt-10 self-center">
+        <CarouselContent className="text-center">
+          <CarouselItem>Jestem</CarouselItem>
+          <CarouselItem>Sobie</CarouselItem>
+          <CarouselItem>Karuzelą</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
